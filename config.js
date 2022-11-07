0483220eb0edb2e55632d2b1e9f5c432335da7ad
Thiệu Trung Kiên <87187870+TrunqKj3n@users.noreply.github.com>
2022-11-07 18:33:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "id": "159716636748232",
-    "cookies": 'sb=2XpfY7yu_3DtasHuCFUfuXp0; datr=2XpfYxG9TBAKuFs7opoB5Cgp; locale=vi_VN; c_user=100081296881500; xs=43:gSZh9N3jzRILZg:2:1667820127:-1:-1; fr=0JeAeaF4ECFiekUue.AWWsO8y-AdObxa5DajS5orXl3GQ.BjaOPa.0D.AAA.0.0.BjaOph.AWV4zJSHEFU; presence=C{"t3":[],"utc3":1667820140261,"v":1}; wd=805x657'
+    "id": "", // ID BÀI VIẾT
+    "cookies": '' // COOKIES ACC FACEBOOK
 }
